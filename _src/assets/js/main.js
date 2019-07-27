1767fe115111159cbd9ff6bca16de7bde2cf2a72
Mariasola <mariasolahornedo@gmail.com>
2019-07-27 20:07:13 +0200

    fill in form Js finished

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,31 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+const changeText = function(ev) {
+  const inputElement = ev.target;
+  const className = inputElement.classList[0];
+  const destElement = document.querySelector(".js-card ." + className);
+  if (destElement === null) {
+    return;
+  }
+
+  const prefix = destElement.dataset.prefix || "";
+  const inputValue = ev.target.value || "";
+  const value = inputValue ? prefix + inputValue : "";
+
+  if (destElement.tagName === "A") {
+    if (value === "") {
+      destElement.href = destElement.dataset.placeholder || "";
+    } else {
+      destElement.href = value;
+    }
+  } else {
+    if (value === "") {
+      destElement.innerHTML = destElement.dataset.placeholder || "";
+    } else {
+      destElement.innerHTML = value;
+    }
+  }
+};
+
+const container = document.querySelector(".fill-in_items");
+container.addEventListener("keyup", changeText);
