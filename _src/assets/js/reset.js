96b81d6872884257941aba57daca6597aa7288cf
sandracubero <sandracuberor@gmail.com>
2019-08-02 01:35:24 +0200

    Add reset functionality to all form items, remaining palette default

--- _src/assets/js/reset.js
+++ _src/assets/js/reset.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const buttonReset = document.querySelector('.js-reset');
+
+
 const resetInfo = function(ev){
   ev.preventDefault();
   //Nombre
@@ -9,6 +11,22 @@ const resetInfo = function(ev){
   //Puesto
   document.querySelector('.js-job').innerHTML = 'Front-end developer';
   document.getElementById('job').value = '';
-  //
+  //Foto
+  profileImage.style.backgroundImage = '';
+  profilePreview.style.backgroundImage = '';
+  //Móvil
+  document.querySelector('.js-phone').href = '';
+  document.getElementById('phone').value = '';
+  //Email
+  document.querySelector('.js-email').href = '';
+  document.getElementById('email').value = '';
+  //Likedin
+  document.querySelector('.js-linkedin').href = '';
+  document.getElementById('linkedin').value = '';
+  //Github
+  document.querySelector('.js-github').href = '';
+  document.getElementById('github').value = '';
 };
+
+
 buttonReset.addEventListener('click', resetInfo);
