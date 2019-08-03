dfd825c068f336e9a3f3523fea45ee1aa7ab2d3a
sandracubero <sandracuberor@gmail.com>
2019-08-03 18:22:12 +0200

    Clean code, delete old comments

--- _src/assets/js/reset.js
+++ _src/assets/js/reset.js
@@ -40,30 +40,4 @@ const resetInfo = function(ev){
   cardPage.classList.add('palette1');
 };
 
-// const resetInfo = function(ev){
-//   ev.preventDefault();
-//   //Nombre
-//   document.querySelector('.js-name').innerHTML = 'Nombre Apellido';
-//   document.getElementById('full_name').value = '';
-//   //Puesto
-//   document.querySelector('.js-job').innerHTML = 'Front-end developer';
-//   document.getElementById('job').value = '';
-//   //Foto
-//   profileImage.style.backgroundImage = '';
-//   profilePreview.style.backgroundImage = '';
-//   //Móvil
-//   document.querySelector('.js-phone').href = '';
-//   document.getElementById('phone').value = '';
-//   //Email
-//   document.querySelector('.js-email').href = '';
-//   document.getElementById('email').value = '';
-//   //Likedin
-//   document.querySelector('.js-linkedin').href = '';
-//   document.getElementById('linkedin').value = '';
-//   //Github
-//   document.querySelector('.js-github').href = '';
-//   document.getElementById('github').value = '';
-// };
-
-
 buttonReset.addEventListener('click', resetInfo);
