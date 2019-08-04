22950da0c05110ad3d714272a07125d50cc7a0a2
Ana Amaro <amarovazquezana@gmail.com>
2019-08-04 23:00:21 +0200

    Improve & check share.js

--- _src/assets/js/share.js
+++ _src/assets/js/share.js
@@ -1,14 +1,41 @@
-/* eslint-disable quotes */
-/* eslint-disable no-console */
+/* eslint-disable curly */
+/* eslint-disable semi */
 /* eslint-disable indent */
-/* eslint-disable no-undef */
+/* eslint-disable no-console */
 /* eslint-disable strict */
+
+// definimos variables//
 const buttonShare = document.querySelector('.share_button_img');
+var responseURL = document.querySelector('.response');
+var fillItems = document.querySelector('.fill-in_items');
+var fr = new FileReader();
 
-const handlerButtonShare = () => sendRequest(userInfo);
 
-buttonShare.addEventListener('click', handlerButtonShare);
+// definimos variable que va a ser una función para mandar una petición a la api (fetch) dónde le pasamos la información del usuario //
+//const handlerButtonShare = () => sendRequest(userInfo);
 
+// Añadimos evento Listener al hacer click en el botón //
+buttonShare.addEventListener('click', sendRequest);
+
+function sendData() {
+    var inputs = Array.from(fillItems);
+    var userInfo = getJSONFromInputs(inputs);
+    userInfo.skills = ['JavaScript', 'React'];
+    userInfo.photo = fr.result;
+    sendRequest(userInfo);
+}
+
+fr.addEventListener('load', sendData);
+
+function getJSONFromInputs(inputs) {
+    return inputs.reduce(function (acc, val) {
+        if (val.nodeName !== 'BUTTON')
+            acc[val.name] = val.value;
+        return acc;
+    }, {})
+}
+
+// función sendRequest //
 function sendRequest(userInfo) {
     buttonShare.disabled = true;
 
@@ -22,7 +49,7 @@ function sendRequest(userInfo) {
         .then(function (resp) {
             buttonShare.disabled = false;
             return resp.json();
-        })
+        }) // entonces llamamos a la función showURL //
         .then(function (result) {
             showURL(result);
         })
@@ -31,17 +58,18 @@ function sendRequest(userInfo) {
         });
 }
 
+// función showURL//
 function showURL(result) {
-    const completedURL = document.querySelector('.completed_content_url');
+    //const responseURL = document.querySelector('.completed_content_url');
     const twitterBtn = document.querySelector('.completed_content_button');
     const twitterLink = document.querySelector('.completed_content_twitter');
-    const tweet = "https://twitter.com/intent/tweet?text";
+    const tweet = 'https://twitter.com/intent/tweet?text';
 
     if (result.succes) {
-        completedURL.innerHTML = '<a href=' + result.cardURL + '>' + result.cardURL + '</a>';
+        /*completedURL*/responseURL.innerHTML = '<a href=' + result.cardURL + '>' + result.cardURL + '</a>';
         twitterBtn.classList.remove('hidden');
         twitterLink.href = tweet + result.cardURL;
     } else {
-        completedURL.innerHTML = 'ERROR:' + result.error;
+        /*completedURL*/responseURL.innerHTML = 'ERROR:' + result.error;
     }
 }
