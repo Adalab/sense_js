2380cff5e1fdc9e7b24454e3fde8e6f7abbfbd71
isabel pizarro <chabelipizarro1@gmail.com>
2019-08-06 20:49:03 +0200

    add open

--- _src/assets/js/collapsible.js
+++ _src/assets/js/collapsible.js
@@ -18,27 +18,43 @@
 
 const trigger = document.querySelectorAll(".js-collapsible_trigger");
 const arrow = document.querySelectorAll(".js-arrow");
+const content = document.querySelectorAll(".js-collapsible_content");
 // for (let i = 0; i < trigger.lenght; i = i + 1) {
 // const alltriggers = trigger[i];
 //}
 
-const content = document.querySelectorAll(".js-collapsible_content");
-for (const allContent of content) {
-  for (const allArrow of arrow) {
-    function openAndClose(ev) {
-      if (
-        allContent.classList.contains("js-collapsible_content") ||
-        allArrow.classList.contains("js-arrow")
-      ) {
-        allContent.classList.remove("js-collapsible_content") ||
-          allArrow.classList.remove("js-arrow");
-      } else {
-        allContent.classList.add("js-collapsible_content") ||
-          allArrow.classList.add("js-arrow");
-      }
-    }
-    for (const alltrigger of trigger) {
-      alltrigger.addEventListener("click", openAndClose);
-    }
+function openAndClose(ev) {
+  for (const contentItem of content) {
+    contentItem.classList.remove("open");
   }
+  // const current = event.currentTarget;
+
+  // for (const contentItem of content) {
+  //   contentItem.classList.remove("open");
+  // }
+  for (const triggerItem of trigger) {
+    triggerItem.classList.remove("open");
+  }
+  // for (let i = 0; i < trigger.length; i++) {
+  //   if (trigger[i] === current) {
+  //     trigger[i].classList.add("open");
+  //     content[i].classList.add("open");
+  //   }
+  // }
+
+  //current.classList.toggle ('js-collapsible_content');
+  if (
+    contentItem.classList.contains("js-collapsible_content") ||
+    allArrow.classList.contains("js-arrow")
+  ) {
+    contentItem.classList.remove("js-collapsible_content") ||
+      allArrow.classList.remove("js-arrow");
+  } else {
+    contentItem.classList.add("js-collapsible_content") &&
+      allArrow.classList.add("js-arrow");
+  }
+}
+
+for (const alltrigger of trigger) {
+  alltrigger.addEventListener("click", openAndClose);
 }
