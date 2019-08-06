ad045ecf073d72363d46a137b6b878dba6cf7add
isabel pizarro <chabelipizarro1@gmail.com>
2019-08-06 13:39:47 +0200

    fix arrows

--- _src/assets/js/collapsible.js
+++ _src/assets/js/collapsible.js
@@ -16,24 +16,29 @@
 //     transform: rotate(-180deg);
 //   }
 
-const trigger = document.querySelector(".js-collapsible_trigger");
-const arrow = document.querySelector(".js-arrow");
-//for (let i = 0; i < trigger.lenght; i = i + 1) {
-// allcollapsibles = trigger[i];
+const trigger = document.querySelectorAll(".js-collapsible_trigger");
+const arrow = document.querySelectorAll(".js-arrow");
+// for (let i = 0; i < trigger.lenght; i = i + 1) {
+// const alltriggers = trigger[i];
 //}
-const content = document.querySelector(".js-collapsible_content");
 
-function openAndClose(ev) {
-  if (
-    content.classList.contains("js-collapsible_content") ||
-    arrow.classList.contains("js-arrow") 
-  ) {
-    content.classList.remove("js-collapsible_content") ||
-      arrow.classList.remove("js-arrow");
-  } else {
-    content.classList.add("js-collapsible_content") ||
-      arrow.classList.add("js-arrow");
+const content = document.querySelectorAll(".js-collapsible_content");
+for (const allContent of content) {
+  for (const allArrow of arrow) {
+    function openAndClose(ev) {
+      if (
+        allContent.classList.contains("js-collapsible_content") ||
+        allArrow.classList.contains("js-arrow")
+      ) {
+        allContent.classList.remove("js-collapsible_content") ||
+          allArrow.classList.remove("js-arrow");
+      } else {
+        allContent.classList.add("js-collapsible_content") ||
+          allArrow.classList.add("js-arrow");
+      }
+    }
+    for (const alltrigger of trigger) {
+      alltrigger.addEventListener("click", openAndClose);
+    }
   }
 }
-
-trigger.addEventListener("click", openAndClose);
