9a536f930601827aed45760ab71d5786db41de87
Ana Amaro <amarovazquezana@gmail.com>
2019-08-07 12:24:29 +0200

    checking disable share_button

--- _src/assets/js/share.js
+++ _src/assets/js/share.js
@@ -1,59 +1,68 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable curly */
-/* eslint-disable semi */
-/* eslint-disable indent */
-/* eslint-disable no-console */
-/* eslint-disable strict */
+'use strict';
 
-// Definimos variables//
+// Variables para el botón de crear tarjeta + link de la tarjeta generada //
 const buttonShare = document.querySelector('.share_button_img');
 const responseURL = document.querySelector('.completed_content_url');
 
+// variables del formulario para el userInfo //
 const fullName = document.querySelector('#full_name');
 const jobTitle = document.querySelector('#job');
-const imagePhoto = document.querySelector('.js__profile-image');
 let phone = document.querySelector('#phone');
 let email = document.querySelector('#email');
 let linkedin = document.querySelector('#linkedin');
 let github = document.querySelector('#github');
 
-const twitterBtn = document.querySelector('.completed_content_button');
+// variables para Twitter //
 const twitterUrl = document.querySelector('.completed_content_button_twitter');
-const tweet = "https://twitter.com/intent/tweet?text=My awesome profile card: ";
+const tweet = 'https://twitter.com/intent/tweet?text=My awesome profile card: ';
 
 // Función que recoge la información del usuario y llama a la api externa (cards) para que nos genere la tarjeta de visita. //
 function writeURL() {
-    const userInfo = {
-        palette: document.querySelector('.form_item:checked').value,
-        name: fullName.value,
-        job: jobTitle.value,
-        phone: phone.value,
-        email: email.value,
-        linkedin: linkedin.value,
-        github: github.value,
-        photo: fr.result,
-    };
-    fetch('https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/card/', {
-        method: 'POST',
-        body: JSON.stringify(userInfo),
-        headers: {
-            'content-type': 'application/json'
-        },
-    })
-        .then(response => response.json())
-        .then(userInfo => showURL(userInfo))
-        .catch(function (error) { console.log(error); });
+  const userInfo = {
+    palette: document.querySelector('.form_item:checked').value,
+    name: fullName.value,
+    job: jobTitle.value,
+    phone: phone.value,
+    email: email.value,
+    linkedin: linkedin.value,
+    github: github.value,
+    // eslint-disable-next-line no-undef
+    photo: fr.result
+  };
+  fetch('https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/card/', {
+    method: 'POST',
+    body: JSON.stringify(userInfo),
+    headers: {
+      'content-type': 'application/json'
+    }
+  })
+    .then(response => response.json())
+    .then(userInfo => showURL(userInfo))
+    // eslint-disable-next-line no-console
+    .catch(error => console.log(error));
 }
 
 // Función showURL //
 function showURL(userInfo) {
-    if (userInfo.success) {
-        responseURL.innerHTML = '<a href=' + userInfo.cardURL + '>' + userInfo.cardURL + '</a>';
-        twitterUrl.href = tweet + userInfo.cardURL;
-    } else {
-        responseURL.innerHTML = 'ERROR:' + userInfo.error;
-    }
+  if (userInfo.success) {
+    responseURL.innerHTML =
+      '<a href=' + userInfo.cardURL + '>' + userInfo.cardURL + '</a>';
+    twitterUrl.href = tweet + userInfo.cardURL;
+  } else {
+    responseURL.innerHTML = 'ERROR:' + userInfo.error;
+  }
+}
+
+// función de preuba para el botón de crear tarjeta para deshabilitarlo una vez pulsado //
+function buttonDisabledEnabled() {
+  event.preventDefault();
+  buttonShare.classList.add('');
+  buttonShare.setAttribute('disabled', true);
+  writeURL();
 }
 
 // Añadimos evento Listener al hacer click en el botón y nos pinte la función writeURL //
 buttonShare.addEventListener('click', writeURL);
+
+// prueba función buttonDisableEnabled //
+buttonShare.addEventListener('click', buttonDisabledEnabled);
