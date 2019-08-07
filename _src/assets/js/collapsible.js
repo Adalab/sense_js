4816df5ce30d79bba9ece3f4bb7eb5842f6bc11f
Samantha Cancino <samanthacancino@gmail.com>
2019-08-07 20:37:05 +0200

    Finish JS for Collapsable and margin of background

--- _src/assets/js/collapsible.js
+++ _src/assets/js/collapsible.js
@@ -3,15 +3,14 @@ const collapsibleTriggers = document.querySelectorAll('.js-collapsible-trigger')
 
 function updateCollapsible(event) {
   const currentCollapsible = event.currentTarget.parentElement;
-  if (currentCollapsible.classList.contains('js-collapsible-open')) {
-    currentCollapsible.classList.remove('js-collapsible-open');
+  const currentCollapsibleStatus = currentCollapsible.classList.contains('js-collapsible-open');
+
+  for (const item of collapsibleTriggers) {
+    item.parentElement.classList.remove('js-collapsible-open');
   }
-  else {
-    for (const item of collapsibleTriggers) {
-      item.parentElement.classList.remove('js-collapsible-open');
-    }
+  if (currentCollapsibleStatus === false) {
+    currentCollapsible.classList.add('js-collapsible-open');
   }
-  currentCollapsible.classList.add('js-collapsible-open');
 }
 
 for (const item of collapsibleTriggers) {
